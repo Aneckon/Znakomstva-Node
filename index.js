abd67caa041ff3fa3714913488b81bccd12bafe0
Aneckon <aneckogri@gmail.com>
2022-09-02 07:40:45 +0300

    Add: client

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const fileUpload= require('express-fileupload')
-const sequelize = require('./db')
-const router = require('./routes/index')
-const path = require("path");
+require('dotenv').config();
+const { Client } = require('pg');
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+const sequelize = require('./db');
+const router = require('./routes/index');
+const path = require('path');
 
+const PORT = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3000
+const app = express();
 
-const app = express()
-
-
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(cookieParser())
-app.use(fileUpload())
-app.use('/api', router)
-
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(cookieParser());
+app.use(fileUpload());
+app.use('/api', router);
 
 app.get('/', (req, res) => {
-    res.send('working')
-})
+  res.send('working');
+});
 
+const client = new Client({
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  ssl: false,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 const start = async () => {
-    try {
-        await sequelize.authenticate()
-        await sequelize.sync()
-        console.log('Connection has been established successfully.');
-        app.listen(PORT, () => {console.log(`server running on port ${PORT}`)})
-    } catch(e) {
-        console.log(e)
-    }
-}
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log('Connection has been established successfully.');
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-start()
+start();
