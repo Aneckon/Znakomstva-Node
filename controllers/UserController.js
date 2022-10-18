80e0e982cbb5c8504e9a5c6f37857c76f2a37b49
Aneckon <aneckogri@gmail.com>
2022-10-18 10:23:07 +0300

    fix: images

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -54,7 +54,10 @@ class UserController {
   }
 
   async addInfo(req, res) {
-    const { id, name, height, birthday, gender, weight, hairColor, eyeColor, datePurpose, aboutMyself, mainProfilePicture } = req.body;
+    const { id, name, height, birthday, gender, weight, hairColor, eyeColor, datePurpose, aboutMyself } = req.body;
+    const { mainProfilePicture } = req.files;
+    let filename = uuid.v4() + '.jpg';
+    mainProfilePicture.mv(path.resolve(__dirname, '..', 'static', filename));
     const user = await User.update(
       {
         name,
@@ -66,7 +69,7 @@ class UserController {
         eyeColor,
         datePurpose,
         aboutMyself,
-        mainProfilePicture,
+        mainProfilePicture: filename,
       },
       { where: { id } },
     );
